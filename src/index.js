10f6c489269ae30a9a570d685e26438e66c6d153
nikhilrnooli <nikhilrnooli@gmail.com>
2025-07-30 11:36:23 +0530

    all setup configure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({path: './env'}) -> not good way to do
 import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
+import {app} from "./app.js"
 
 dotenv.config({
     path: './env'
